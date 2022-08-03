0fc567cfce91fd72e93521c4536a269428967f3a
Andres Ordoñez <ordonezandres202@gmail.com>
2022-08-03 14:03:28 -0500

    cambios

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,48 +6,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <title>salud</title>
 </head>
 <body>
-    <header class="container-fluid d-flex mw-100 border pt-2 ">
-        <div class="container d-flex ms-5">
-               <h2 class="">Salud</h2>
-        </div>
-        <div class="container w-50 d-flex justify-content-between ">
-            <div class="d-flex form-group">
-                <input type="text" class="form-control h-75 w-75 input-group">
-                <button class="btn btn-group h-75 btn-primary">Buscar</button>
-            </div>
-             <div class="d-flex ps-2">
+   
+        <div class="container ff">           
+             <div class="ps-2">
                   <div class="form-group">
-                    <select id="my-select" class="form-control" name="">
-                      <option selected>juan camilo yonda</option>
-                      <option value=""><a href="detail_perfil.html">Ajustes</a></option>
-                      <option value="" >salir</option>
-                    </select>
+                    <a class="bi bi-box-arrow-left g" href="{{route('cerrarSesion')}}"></a>
                   </div>
              </div>
         </div>
-    </header>
+   
     <section class="body container-fliud d-flex col-12">
         @include('sweetalert::alert')
-           <section class="col-2 d-flex  border flex-column" style="height: 100vh;">
+           <section class="col-2 d-flex  flex-column" style="height: 100vh;">
                  <div class="w-100 d-flex p-5  justify-content-center">
-                    <h1 class="font-1 active">S</h1>
-                    <h2>Bienvenido . {{auth()->user()->name}} </h2>
-                    <img src="{{asset("storage/".auth()->user()->avatar)}}"/>
+
+                    <h5 class="font-1 active d-flex justify-content-center t">{{auth()->user()->name}} </h5>
+
+                    {{-- <img src="{{asset("storage/".auth()->user()->avatar)}}"/> --}}
                  </div>
-                 <nav class="d-flex flex-column nav w-100 p-4 h-50 justify-content-between">
-                    <a class=" nav-link d-flex justify-content-center">Home</a>
-                    <a class="nav-link d-flex justify-content-center">calendario</a>
-                    <a class="nav-link d-flex justify-content-center">histotial medico</a>
-                    <a class="nav-link d-flex justify-content-center">Ajustes</a>
-                    <a href="{{route('cerrarSesion')}}">Cerrar Sesion</a>
+                 <nav class="d-flex flex-wrap flex-column nav w-100 p-4 mt-5 h-50 justify-content-between">
+                    <a class="nav-link d-flex justify-content-center t" href="#">Home</a>
+                    <a class="nav-link d-flex justify-content-center t">calendario</a>
+                    <a class="nav-link d-flex justify-content-center t">histotial medico</a>
+                    <a class="nav-link d-flex justify-content-center t">Ajustes</a>
+                    
                  </nav>
            </section>
-           <section class="col-10 d-flex border">
-            <div class="container card col-11 m-3 ">
+           <section class="col-12 d-flex border">
+            <div class="container-fluid card col-12">
                    @yield('content')
             </div>
                  
@@ -60,6 +51,33 @@
 </body>
 
 <style>
+
+    *{
+        margin: 0px;
+        top: 0;
+        bottom: 0;
+    }
+
+    body{
+        background-color: #437176;
+    }
+    .g{
+        width: 10%;
+        height: 50px;
+    }
+
+    header{
+        height: 90px;
+        background-color: #FFFF
+    }
+
+    .ff{
+        display: flex;
+        justify-content: flex-end;
+        height: 60px;
+        padding: 0px;
+
+    }
     .back1 {
         background-color: #69A6A4
     }
@@ -67,6 +85,11 @@
     .btn1 {
         background-color: #DC5716;
         color: #FFFF
+    }
+
+    .t{
+        font-size: 30px;
+        margin-top: 0px
     }
 </style>
 </html>
